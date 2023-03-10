cf890841d4c4e9aaf4ca1bcde36a29d152ba13b6
yunjjung <yjung203@naver.com>
2023-03-10 09:07:03 +0900

    [ADD]규칙 추가

--- src/pages/Rule/index.jsx
+++ src/pages/Rule/index.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Rule() {
-  return <div>규칙</div>;
+  return(
+    <div>
+      <div className="rule">
+        <h1>규칙</h1>
+        1. 잔디 매일 심기<br></br>
+        2. 싸우지 말기<br></br>
+        3. 모르면 화내지 말고 알려주기<br></br>
+        4. 시간 약속 지키기<br></br>
+        5. 개인 행동 금지<br></br>
+        6. 비난 금지<br></br>
+        7. 사이좋게 지내기<br></br>
+        </div>
+    
+    </div>
+  ); 
 }
