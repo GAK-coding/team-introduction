f2c3f7739ca5700ff779a55b039910927f3f3093
Chaeyoung <oco6029@naver.com>
2023-03-11 15:39:00 +0900

    catchphrase

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Vision } from "./styles";
+import { Catchphrase, Vision } from "./styles";
 
 export default function Home() {
   return (
@@ -7,6 +7,11 @@ export default function Home() {
       <Vision>
         <p>Go At Kernel</p> <img src="main_logo.png" alt="gak"></img>
       </Vision>
+
+      <Catchphrase>
+        <p>도전 GAK, 빌드 GAK, 배포 GAK</p>
+        <div />
+      </Catchphrase>
     </>
   );
 }
--- src/pages/Home/styles.jsx
+++ src/pages/Home/styles.jsx
@@ -5,15 +5,39 @@ export const Vision = styled.div`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  margin-top: 8ex;
+  margin-top: 60px;
 
   img {
     width: 250px;
-    margin-left: 5ex;
+    margin-left: 74px;
   }
 
   p {
-    font-size: 70px;
+    font-size: 60px;
     color: #464646;
+  }
+`;
+
+export const Catchphrase = styled.div`
+  position: relative;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 350px;
+
+  p {
+    font-size: 35px;
+    color: #464646;
+    z-index: 2;
+  }
+
+  div {
+    position: absolute;
+    width: 510px;
+    height: 20px;
+    bottom: 33px;
+    background: rgba(244, 229, 194, 0.55);
+    z-index: 1;
   }
 `;
