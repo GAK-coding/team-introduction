1a3ce9e3112163033e1138a08795d099063e1c98
5hyun <qhslsl@gmail.com>
2023-03-16 11:56:08 +0900

    Feat: 첫번째 풀 머지

--- src/pages/MakeForm/index.jsx
+++ src/pages/MakeForm/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Anchor, Col, Row } from "antd";
 import FormInfo from "../../components/FormInfo";
+import { SortablePane, Pane } from "react-sortable-pane";
 
 export default function MakeForm() {
   const items = [
@@ -21,12 +22,31 @@ export default function MakeForm() {
     // },
   ];
 
+  const a = <div style={{ border: "1px solid" }}>1</div>;
+  const b = <div style={{ border: "1px solid" }}>2</div>;
+
+  const panes = [a, b].map((key) => (
+    <Pane key={key} defaultSize={{ width: "100%", height: 120 }}>
+      {/*<p>00{key}</p>*/}
+      <p>
+        {key}
+        {a}
+      </p>
+    </Pane>
+  ));
+
   return (
     <div>
       <Row>
         <Col span={3} />
         <Col span={18} style={{ padding: "0 2rem" }}>
           <FormInfo />
+
+          <div style={{ padding: "10px" }}>
+            <SortablePane direction="vertical" margin={20}>
+              {panes}
+            </SortablePane>
+          </div>
 
           {/*<div id="part-1" style={{ height: "50vh", background: "red" }} />*/}
           {/*<div*/}
