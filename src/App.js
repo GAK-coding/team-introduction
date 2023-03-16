beb5be03846be03fe5052ca842585569d3981c7c
5hyun <qhslsl@gmail.com>
2023-03-16 12:18:10 +0900

    Styles: 폰트

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import Home from "./pages/Home";
 import Info from "./pages/Info";
 import Rule from "./pages/Rule";
 import Each from "./pages/Each";
 import Header from "./components/Header";
-
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path={"info"} element={<Info />} />
-        <Route path={"info/:name"} element={<Each />} />
-        <Route path={"rule"} element={<Rule />} />
-      </Routes>
+      <div className={"App"}>
+        <Header />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path={"info"} element={<Info />} />
+          <Route path={"info/:name"} element={<Each />} />
+          <Route path={"rule"} element={<Rule />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
