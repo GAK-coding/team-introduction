2305a40c2f7fd88ebb77ee56da83c06bf25ec89b
Chae Young <oco6029@naver.com>
2023-03-19 15:46:37 +0900

    forn - 응답페이지

--- src/pages/Form/index.jsx
+++ src/pages/Form/index.jsx
@@ -1,45 +1,110 @@
 import { useState } from "react";
-import { CheckBox, Main } from "./styles";
+import { ContentCh, ContentRa, ContentTe, Main, Title } from "./styles";
 
 export default function Form() {
-  const answer = [{}];
-
   const checkboxList = [
-    { name: "사과" },
-    { name: "딸기" },
-    { name: "토마토" },
-    { name: "포도" },
-    { name: "귤" },
+    { id: 0, name: "서울" },
+    { id: 1, name: "인천" },
+    { id: 2, name: "경기" },
+    { id: 3, name: "경북" },
+    { id: 4, name: "경남" },
+    { id: 5, name: "전북" },
+    { id: 6, name: "전남" },
+    { id: 7, name: "제주" },
   ];
 
-  const [checkedbox, setCheckebox] = useState(false);
+  // 객관식 input태그 상태관리
+  const [checkedList, setCheckedList] = useState([]);
 
-  const onCheckedItem = ({ target }) => {
-    if (target.checked) {
-      setCheckebox(true);
-      answer.push({ type: "checkbox", value: target.id });
-      console.log(answer);
-    } else {
-      setCheckebox(false);
+  // 체크시 데이터 저장, 해제시 데이터 삭제
+  const onCheckedElement = (checked, item) => {
+    if (checked) {
+      setCheckedList([...checkedList, item]);
+      console.log(item, " 이 체크 되었다");
+    } else if (!checked) {
+      setCheckedList(checkedList.filter((el) => el !== item));
+      console.log(item, " 이 체크가 취소되었다");
     }
   };
 
-  return (
-    <>
-      <h1>객관식</h1>
-      {checkboxList.map((item) => (
-        <label clasName="checkboxLabel" key={item.name}>
-          <input
-            type="checkbox"
-            id={item.name}
-            onChange={(e) => {
-              onCheckedItem(e);
-            }}
-          />
+  // // 해제시 데이터 삭제
+  // const onRemove = (item) => {
+  //   setCheckedList(checkedList.filter((el) => el !== item));
+  // };
 
-          <span>{item.name}</span>
+  // 주관식 input태그 상태관리
+  const [text, setText] = useState("");
+
+  const onChangeText = (e) => {
+    console.log(e.target.value);
+    setText(e.target.value);
+  };
+
+  // 찬부식 input 태그 상태관리
+  const [radio, setRadio] = useState([]);
+
+  const onSelect = (item) => {
+    setRadio(item.target.value);
+    console.log(item.target.value, "로 선택 되었다");
+  };
+
+  return (
+    <Main>
+      <Title>
+        <p>지역을 선택해주세요</p> <hr />
+      </Title>
+      <ContentCh>
+        {checkboxList.map((item) => (
+          <label clasName="checkboxLabel" key={item.id}>
+            <input
+              type="checkbox"
+              value={item.name}
+              onChange={(e) => {
+                onCheckedElement(e.target.checked, e.target.value);
+              }}
+              checked={checkedList.includes(item.name) ? true : false}
+            />
+            <span>{item.name}</span>
+          </label>
+        ))}
+      </ContentCh>
+
+      <Title>
+        <p>의견을 입력해주세요</p> <hr />
+      </Title>
+
+      <ContentTe>
+        <label className="textLabel">
+          <span>답변 :</span>
+          <input type="text" onChange={onChangeText} value={text}></input>
         </label>
-      ))}
-    </>
+        <br />
+        <br />
+        <b> 입력 값 : {text}</b>
+      </ContentTe>
+
+      <Title>
+        <p>성별을 골라주세요</p> <hr />
+      </Title>
+
+      <ContentRa>
+        <label className="radio">
+          <input
+            type="radio"
+            value="male"
+            checked={radio === "male"}
+            onChange={onSelect}
+          />
+          <span>남자</span>
+          <input
+            type="radio"
+            value="female"
+            checked={radio === "female"}
+            onChange={onSelect}
+          />
+          <span>여자</span>
+        </label>
+      </ContentRa>
+    </Main>
   );
 }
---
+++ src/pages/Form/styles.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  /* display: flex; */
+  /* justify-content: center; */
+  /* align-items: center; */
+  margin-left: 40px;
+  margin-top: 40px;
+  margin-right: 80px;
+`;
+
+export const Title = styled.div`
+  margin-top: 40px;
+  p {
+    font-size: 40px;
+    color: #464646;
+  }
+`;
+
+export const ContentCh = styled.div`
+  label {
+    margin: 3px 10px 10px 10px;
+  }
+
+  input {
+    margin: 3px 10px 10px 10px;
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+  }
+`;
+
+export const ContentTe = styled.div`
+  input {
+    margin: 3px 10px 10px 15px;
+    width: 220px;
+    height: 30px;
+  }
+`;
+
+export const ContentRa = styled.div`
+  label {
+    margin: 3px 10px 10px 10px;
+  }
+
+  input {
+    margin: 3px 10px 10px 10px;
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+  }
+`;
